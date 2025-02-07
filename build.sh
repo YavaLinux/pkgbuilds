9768bc98310112decf165501b61add67482fd398
Hamed Mahmoudkhani <ainyava@gmail.com>
2025-02-07 15:16:04 +0330

    chore: update build script

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 # Maintainer: Hamed Mahmoudkhani <ainyava+distro@gmail.com>
-if ! [[ $1 ]] then
-    echo "Usage: sh ./build.sh [OUTPUT_DIR]"
-    exit
-fi
-
-for pkg in ./*/
-do
-    echo "Building the package $pkg"
-    cd $pkg
-    yes | makepkg -s
-    cp *.pkg.tar.zst ../../archrepo/x86_64/ $1
-    cd ..
+for pkg in ./*/; do
+  echo "Building the package $pkg"
+  cd $pkg
+  yes | makepkg -s
+  cp *.pkg.tar.zst ../../repo/x86_64/
+  cd ..
 done
